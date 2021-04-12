bd8c1303e162271b5efb1827ff2ce0202ee0e03b
szabolcsit <szabolcsit@hu.ibm.com>
2021-04-12 09:28:08 +0200

    feat: option output

--- src/main.go
+++ src/main.go
@@ -1,10 +1,19 @@
 package main
 
 import (
-    "flag"
-    "fmt"
+	"flag"
+	"fmt"
 )
 
-func main () {
-  fmt.Println("Hi!")
+var output string
+
+func init() {
+	flag.StringVar(&output, "output", "json", "Output format, default is 'json'")
+	flag.StringVar(&output, "o", "json", "Output format, default is 'json' (shorthand)")
+}
+
+func main() {
+	flag.Parse()
+
+	fmt.Println("output has value ", output)
 }
